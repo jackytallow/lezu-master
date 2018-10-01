ed9f3a67956418e1c058f960c13f3a182f2641d0
jackytallow <3434481891@qq.com>
2018-10-01 12:37:21 +0800

    第二次提交

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,14 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userinfo:{},
+    houseType:['一室户','两室户','三室户'],
+    houseTypeIndex:0,
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+  //   let that = this
+  //   //获取全局数据
+  //   app.getUserInfo(function(userinfo){
+  //      //更新数据
+  //      that.setData({
+  //         userinfo:userinfo
+  //      })
+  //   })
+  // },
+
+  //改变户型，使用bindPickerChange(e)
+     
+  //  bindPickerChange(e){
+  //    let _this = this;
+  //    _this.setData({
+  //      //获取数值
+  //      houseTypeIndex:e.detail.value
+  //    })
+  //  }
   },
 
   /**
